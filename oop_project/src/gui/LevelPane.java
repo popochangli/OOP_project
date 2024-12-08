38d60eb8465332eb1a5da5ee5f792accefc8df0d
Suphawit <suphawit.promkhot@gmail.com>
2024-12-08 15:57:17 +0700

    add_Level

--- oop_project/src/gui/LevelPane.java
+++ oop_project/src/gui/LevelPane.java
@@ -1,22 +1,20 @@
 package gui;
 
-import javafx.event.EventHandler;
+import item.Level;
 import javafx.geometry.Insets;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 public class LevelPane extends GridPane {
 
-    public LevelPane(){
+    public LevelPane(Level level){
         this.setPrefWidth(428);
         this.setHgap(8);
         this.setPadding(new Insets(4));
 
         Text nameText = new Text("hi");
 
+        this.add(nameText,1,0);
 
     }
 }
--- oop_project/src/gui/LevelSelectPane.java
+++ oop_project/src/gui/LevelSelectPane.java
@@ -1,5 +1,6 @@
 package gui;
 
+import item.Level;
 import javafx.animation.ScaleTransition;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -20,62 +21,62 @@ import java.util.ArrayList;
 public class LevelSelectPane extends VBox{
 
     private static LevelSelectPane instance;
-    private ArrayList<Book> books;
+    private ArrayList<Level> levels;
 
-    private BookListPane() {
-        books = new ArrayList<>();
-        books.add(new Book("Hairy Panner and the professor's stove", "J.K. Kidding", 2, "res/1.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
-        books.add(new Book("Hairy Panner and the Kitchen of secrets", "J.K. Kidding", 3, "res/2.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
-        books.add(new Book("Hairy Panner and the Chef of AsKarnBan", "J.K. Kidding", 1, "res/3.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
-        books.add(new Book("Mercy Johndaughter and the Cloud Thief", "Nick Nornand", 5, "res/4.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
-        books.add(new Book("Mercy Johndaughter and the Land of Heroes", "Nick Nornand", 4, "res/5.png", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
+    private LevelSelectPane() {
+        levels = new ArrayList<>();
+        levels.add(new Level("Hairy Panner and the professor's stove", "J.K. Kidding", 2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
+        levels.add(new Level("Hairy Panner and the Kitchen of secrets", "J.K. Kidding", 3, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
+        levels.add(new Level("Hairy Panner and the Chef of AsKarnBan", "J.K. Kidding", 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
+        levels.add(new Level("Mercy Johndaughter and the Cloud Thief", "Nick Nornand", 5, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
+        levels.add(new Level("Mercy Johndaughter and the Land of Heroes", "Nick Nornand", 4, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula, sem a ultricies scelerisque, eros tellus sodales sem, ut malesuada lorem leo sit amet libero. Donec vel mi posuere, rhoncus purus eget, posuere sem. Mauris vehicula tincidunt turpis, eget malesuada lectus pretium non. Aenean eu egestas erat. Aenean ex ipsum, ornare non tristique in, facilisis ac ex. Vestibulum eget elit et nunc posuere laoreet sed eget dui."));
 
         // TODO: FILL CODE HERE
         this.setFillWidth(true);
         this.setAlignment(Pos.CENTER);
-        this.setSearchedBooks(books);
+        this.showLevels();
     }
 
-    public static BookListPane getInstance() {
+    public static LevelSelectPane getInstance() {
         if (instance == null) {
-            instance = new BookListPane();
+            instance = new LevelSelectPane();
         }
         return instance;
     }
 
-    public ArrayList<Book> getBooks() {
-        return books;
+    public ArrayList<Level> getLevels() {
+        return levels;
     }
 
-    public Button newBookButton(){
-        Button button = new Button("Add New Book");
-        button.setPrefWidth(400);
-        button.setBackground(new Background(new BackgroundFill(Color.DARKCYAN, null,null)));
-        button.setTextFill(Color.WHITE);
-        EventHandler<MouseEvent> gotoAddNewBookPage = mouseEvent -> Goto.addNewBookPage();
-        button.setOnMouseClicked(gotoAddNewBookPage);
+//    public Button newLevelButton(){
+//        Button button = new Button("Add New Level");
+//        button.setPrefWidth(400);
+//        button.setBackground(new Background(new BackgroundFill(Color.DARKCYAN, null,null)));
+//        button.setTextFill(Color.WHITE);
+//        EventHandler<MouseEvent> gotoAddNewLevelPage = mouseEvent -> Goto.addNewLevelPage();
+//        button.setOnMouseClicked(gotoAddNewLevelPage);
+//
+//        return button;
+//    }
 
-        return button;
-    }
-
-    public void setSearchedBooks(ArrayList<Book> searchedBooks){
+    public void showLevels(){
 
         this.getChildren().clear();
         Background white = new Background(new BackgroundFill(Color.WHITE,null,null));
         Background nearWhite = new Background(new BackgroundFill(Color.color(0,0,0,0.05),null,null));
 
-        for(int i = 0;i < searchedBooks.size();i++){
-            BookPane newBookPane = new BookPane(searchedBooks.get(i));
+        for(int i = 0;i < levels.size();i++){
+            LevelPane newLevelPane = new LevelPane(levels.get(i));
             if(i%2 == 0){
-                newBookPane.setBackground(white);
+                newLevelPane.setBackground(white);
             }else {
-                newBookPane.setBackground(nearWhite);
+                newLevelPane.setBackground(nearWhite);
             }
 
-            this.getChildren().add(newBookPane);
+            this.getChildren().add(newLevelPane);
         }
 
-        this.getChildren().add(newBookButton());
+//        this.getChildren().add(newLevelButton());
     }
 
 }
