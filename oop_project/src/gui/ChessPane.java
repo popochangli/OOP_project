e0865475ac773b80cf633ea7a22479de22562ce9
Praeploy Kiatsuksri <praeploy05@gmail.com>
2024-12-10 09:05:31 +0700

    manual mearge attempt

--- oop_project/src/gui/ChessPane.java
+++ oop_project/src/gui/ChessPane.java
@@ -1,3 +1,126 @@
+//package gui;
+//
+//import javafx.geometry.Insets;
+//import javafx.geometry.Pos;
+//import javafx.scene.image.Image;
+//import javafx.scene.layout.Background;
+//import javafx.scene.layout.BackgroundFill;
+//import javafx.scene.layout.Border;
+//import javafx.scene.layout.BorderStroke;
+//import javafx.scene.layout.BorderStrokeStyle;
+//import javafx.scene.layout.BorderWidths;
+//import javafx.scene.layout.CornerRadii;
+//import javafx.scene.layout.GridPane;
+//import javafx.scene.paint.Color;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class ChessPane extends GridPane {
+//
+//	private ArrayList<TicTacToeCell> allCells;
+//	private static final Map<String, Image> imageCache = new HashMap<>();
+//	private int chessPaneWidth;
+//	private int chessPaneHeight;
+//	
+//	public ChessPane(int width, int height, ArrayList<String> pieces) {
+//		this.chessPaneWidth = width;
+//		this.chessPaneHeight = height;
+//		
+//		this.setHgap(8.0);
+//		this.setVgap(8.0);
+//		this.setPadding(new Insets(8.0));
+//		this.setAlignment(Pos.CENTER);
+//		this.setPrefWidth(500.0);
+//		this.setPrefHeight(500.0);
+//		int cellSize = Math.min(500 / width, 500 / height);
+//		this.setPrefSize((double) (cellSize * width), (double) (cellSize * height));
+//		this.setMaxSize((double) (cellSize * width), (double) (cellSize * height));
+//		this.setMinSize((double) (cellSize * width), (double) (cellSize * height));
+//		this.setBorder(new Border(new BorderStroke[] {
+//				new BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT) }));
+//		this.setBackground(new Background(
+//				new BackgroundFill[] { new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY) }));
+//		this.allCells = new ArrayList<TicTacToeCell>();
+//
+//		int l = 0;
+//		for (int i = 0; i < width; ++i) {
+//			for (int j = 0; j < height; ++j) {
+//				TicTacToeCell cell = new TicTacToeCell(i, j);
+//				cell.setPrefSize((double) cellSize, (double) cellSize);
+//
+//				if (l < pieces.size()) {
+//					String piece = pieces.get(l);
+//					String imagePath = (i + j) % 2 == 0 ? "piece/" + piece + "B.png" : "piece/" + piece + "W.png";
+//
+//					Image pieceImage = loadImage(imagePath);
+//
+//					if (pieceImage != null) {
+//						cell.draw(pieceImage, (i + j) % 2 == 0 ? piece + "B" : piece + "W");
+//					} else {
+//						cell.draw(null, null);
+//					}
+//				}
+//
+//				this.allCells.add(cell);
+//				this.add(cell, i, j);
+//				l++;
+//			}
+//		}
+//
+//	}
+//
+//	
+//
+//	public int getChessPaneWidth() {
+//		return chessPaneWidth;
+//	}
+//
+//
+//
+//	public void setChessPaneWidth(int chessPaneWidth) {
+//		this.chessPaneWidth = chessPaneWidth;
+//	}
+//
+//
+//
+//	public int getChessPaneHeight() {
+//		return chessPaneHeight;
+//	}
+//
+//
+//
+//	public void setChessPaneHeight(int chessPaneHeight) {
+//		this.chessPaneHeight = chessPaneHeight;
+//	}
+//
+//
+//
+//	private Image loadImage(String imagePath) {
+//		if (!imageCache.containsKey(imagePath)) {
+//			imageCache.put(imagePath, new Image(ClassLoader.getSystemResource(imagePath).toString()));
+//		}
+//		return imageCache.get(imagePath);
+//	}
+//
+//	public ArrayList<TicTacToeCell> getAllCells() {
+//		return this.allCells;
+//	}
+//	
+//	public TicTacToeCell getCell(int x, int y) {
+//	    if (x < 0 || x >= chessPaneWidth || y < 0 || y >= chessPaneHeight) {
+//	        //System.out.println("Invalid cell coordinates: (" + x + ", " + y + ")");
+//	        return null;
+//	    }
+//	    //int index = y * chessPaneWidth + x;
+//	    int index = x * chessPaneHeight + y;
+//	    //System.out.println("Retrieving cell at (" + x + ", " + y + ") -> Index: " + index);
+//	    return allCells.get(index);
+//	}
+//
+//	
+//
+//}
 package gui;
 
 import javafx.geometry.Insets;
@@ -12,6 +135,8 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,25 +148,20 @@ public class ChessPane extends GridPane {
 	private int chessPaneWidth;
 	private int chessPaneHeight;
 	
-	public ChessPane(int width, int height, ArrayList<String> pieces) {
-		this.chessPaneWidth = width;
-		this.chessPaneHeight = height;
-		
+	public ChessPane(int width, int height, ArrayList<String> pieces, double panelSize) {
 		this.setHgap(8.0);
 		this.setVgap(8.0);
 		this.setPadding(new Insets(8.0));
 		this.setAlignment(Pos.CENTER);
-		this.setPrefWidth(500.0);
-		this.setPrefHeight(500.0);
-		int cellSize = Math.min(500 / width, 500 / height);
-		this.setPrefSize((double) (cellSize * width), (double) (cellSize * height));
-		this.setMaxSize((double) (cellSize * width), (double) (cellSize * height));
-		this.setMinSize((double) (cellSize * width), (double) (cellSize * height));
-		this.setBorder(new Border(new BorderStroke[] {
-				new BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT) }));
-		this.setBackground(new Background(
-				new BackgroundFill[] { new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY) }));
-		this.allCells = new ArrayList<TicTacToeCell>();
+
+		// Set the preferred size based on panelSize
+		this.setPrefSize(panelSize, panelSize);
+		int cellSize = Math.min((int) panelSize / width, (int) panelSize / height);
+		this.setPrefSize(cellSize * width, cellSize * height);
+		this.setMaxSize(cellSize * width, cellSize * height);
+		this.setMinSize(cellSize * width, cellSize * height);
+
+		this.allCells = new ArrayList<>();
 
 		int l = 0;
 		for (int i = 0; i < width; ++i) {
@@ -67,7 +187,6 @@ public class ChessPane extends GridPane {
 				l++;
 			}
 		}
-
 	}
 
 	
@@ -97,11 +216,17 @@ public class ChessPane extends GridPane {
 
 
 	private Image loadImage(String imagePath) {
-		if (!imageCache.containsKey(imagePath)) {
-			imageCache.put(imagePath, new Image(ClassLoader.getSystemResource(imagePath).toString()));
-		}
-		return imageCache.get(imagePath);
+	    URL resource = ClassLoader.getSystemResource(imagePath);
+	    if (resource == null) {
+	        System.err.println("Resource not found: " + imagePath);
+	        return null;
+	    }
+	    if (!imageCache.containsKey(imagePath)) {
+	        imageCache.put(imagePath, new Image(resource.toString()));
+	    }
+	    return imageCache.get(imagePath);
 	}
+
 
 	public ArrayList<TicTacToeCell> getAllCells() {
 		return this.allCells;
--- oop_project/src/gui/TicTacToeCell.java
+++ oop_project/src/gui/TicTacToeCell.java
@@ -39,6 +39,7 @@ public class TicTacToeCell extends Pane {
 																													// to
 																													// fit
 																													// cell
+
 		);
 
 		this.setBackground(
@@ -66,21 +67,21 @@ public class TicTacToeCell extends Pane {
 		// Drag detected on source cell
 		this.setOnDragDetected(event -> {
 			if (pieceImage == null || pieceType == null || !isWhitePiece(pieceType)) {
-	            System.out.println("Only white pieces can move.");
-	            event.consume();
-	            return;
-	        }
+				System.out.println("Only white pieces can move.");
+				event.consume();
+				return;
+			}
 
-	        Dragboard db = startDragAndDrop(TransferMode.MOVE);
-	        ClipboardContent content = new ClipboardContent();
-	        content.putImage(pieceImage);
-	        content.putString(pieceType);
-	        db.setContent(content);
+			Dragboard db = startDragAndDrop(TransferMode.MOVE);
+			ClipboardContent content = new ClipboardContent();
+			content.putImage(pieceImage);
+			content.putString(pieceType);
+			db.setContent(content);
 
-	        highlightValidMoves();
-	        clearPiece();
-	        event.consume();
-	    });
+			highlightValidMoves();
+			clearPiece();
+			event.consume();
+		});
 
 		// Drag over target cell
 		this.setOnDragOver(event -> {
@@ -92,136 +93,139 @@ public class TicTacToeCell extends Pane {
 
 		this.setOnDragDropped(event -> {
 			Dragboard db = event.getDragboard();
-	        boolean success = false;
+			boolean success = false;
 
-	        if (db.hasImage() && db.hasString()) {
-	            ChessPane chessPane = (ChessPane) getParent();
-	            TicTacToeCell sourceCell = (TicTacToeCell) event.getGestureSource();
-	            int startX = GridPane.getColumnIndex(sourceCell);
-	            int startY = GridPane.getRowIndex(sourceCell);
-	            int targetX = GridPane.getColumnIndex(this);
-	            int targetY = GridPane.getRowIndex(this);
+			if (db.hasImage() && db.hasString()) {
+				ChessPane chessPane = (ChessPane) getParent();
+				TicTacToeCell sourceCell = (TicTacToeCell) event.getGestureSource();
+				int startX = GridPane.getColumnIndex(sourceCell);
+				int startY = GridPane.getRowIndex(sourceCell);
+				int targetX = GridPane.getColumnIndex(this);
+				int targetY = GridPane.getRowIndex(this);
 
-	            if (validateMove(db.getString(), startX, startY, targetX, targetY, chessPane)) {
-	                processMove(db.getImage(), db.getString());
-	                success = true;
-	            } else {
-	                System.out.println("Invalid move for " + db.getString());
-	            }
-	        }
+				if (validateMove(db.getString(), startX, startY, targetX, targetY, chessPane)) {
+					processMove(db.getImage(), db.getString());
+					success = true;
+				} else {
+					System.out.println("Invalid move for " + db.getString());
+				}
+			}
 
-	        event.setDropCompleted(success);
-	        event.consume();
-	    });
+			event.setDropCompleted(success);
+			event.consume();
+		});
 
 		// Drag done on source cell
 		this.setOnDragDone(event -> {
 			clearHighlights();
-	        if (event.getTransferMode() != TransferMode.MOVE) {
-	            revertPiece(event.getDragboard());
-	        }
-	        event.consume();
-	    });
+			if (event.getTransferMode() != TransferMode.MOVE) {
+				revertPiece(event.getDragboard());
+			}
+			event.consume();
+		});
 	}
-	
+
 	private boolean isWhitePiece(String pieceType) {
-        return pieceType.endsWith("W");
-    }
-	
+		return pieceType.endsWith("W");
+	}
+
 	private void highlightValidMoves() {
-        ChessPane chessPane = (ChessPane) getParent();
-        int startX = GridPane.getColumnIndex(this);
-        int startY = GridPane.getRowIndex(this);
+		ChessPane chessPane = (ChessPane) getParent();
+		int startX = GridPane.getColumnIndex(this);
+		int startY = GridPane.getRowIndex(this);
 
 //        if (startX == null || startY == null) {
 //            System.out.println("Source cell coordinates could not be determined.");
 //            return;
 //        }
 
-        for (int x = 0; x < chessPane.getChessPaneWidth(); x++) {
-            for (int y = 0; y < chessPane.getChessPaneHeight(); y++) {
-                TicTacToeCell targetCell = chessPane.getCell(x, y);
-                if (targetCell != null && validateMove(pieceType, startX, startY, x, y, chessPane)) {
-                    if (!targetCell.hasPiece() || !isWhitePiece(targetCell.getPieceType())) {
-                        targetCell.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-                    }
-                }
-            }
-        }
-    }
-	
+		for (int x = 0; x < chessPane.getChessPaneWidth(); x++) {
+			for (int y = 0; y < chessPane.getChessPaneHeight(); y++) {
+				TicTacToeCell targetCell = chessPane.getCell(x, y);
+				if (targetCell != null && validateMove(pieceType, startX, startY, x, y, chessPane)) {
+					if (!targetCell.hasPiece() || !isWhitePiece(targetCell.getPieceType())) {
+						targetCell.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID,
+								CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+					}
+				}
+			}
+		}
+	}
+
 	private void clearHighlights() {
-        ChessPane chessPane = (ChessPane) getParent();
-        for (int x = 0; x < chessPane.getChessPaneWidth(); x++) {
-            for (int y = 0; y < chessPane.getChessPaneHeight(); y++) {
-                TicTacToeCell targetCell = chessPane.getCell(x, y);
-                if (targetCell != null) {
-                    targetCell.setBorder(null);
-                }
-            }
-        }
-    }
-	
+		ChessPane chessPane = (ChessPane) getParent();
+		for (int x = 0; x < chessPane.getChessPaneWidth(); x++) {
+			for (int y = 0; y < chessPane.getChessPaneHeight(); y++) {
+				TicTacToeCell targetCell = chessPane.getCell(x, y);
+				if (targetCell != null) {
+					targetCell.setBorder(null);
+				}
+			}
+		}
+	}
+
 	private void revertPiece(Dragboard db) {
-        if (db.hasImage() && db.hasString()) {
-            draw(db.getImage(), db.getString());
-        }
-    }
-	
+		if (db.hasImage() && db.hasString()) {
+			draw(db.getImage(), db.getString());
+		}
+	}
+
 	private void processMove(Image pieceImage, String pieceType) {
-        if (hasPiece()) {
-            char currentColor = getPieceType().charAt(getPieceType().length() - 1);
-            char incomingColor = pieceType.charAt(pieceType.length() - 1);
-            if (currentColor != incomingColor) {
-                System.out.println("Drop succeeded: Opposite team piece replaced.");
-                draw(pieceImage, pieceType);
-            } else {
-                System.out.println("Drop failed: Same team pieces.");
-            }
-        } else {
-            System.out.println("Drop succeeded: Moved piece to empty cell.");
-            draw(pieceImage, pieceType);
-        }
-    }
-	
-	private boolean validateMove(String pieceType, int startX, int startY, int targetX, int targetY, ChessPane chessPane) {
-        int dx = targetX - startX;
-        int dy = targetY - startY;
+		if (hasPiece()) {
+			char currentColor = getPieceType().charAt(getPieceType().length() - 1);
+			char incomingColor = pieceType.charAt(pieceType.length() - 1);
+			if (currentColor != incomingColor) {
+				System.out.println("Drop succeeded: Opposite team piece replaced.");
+				draw(pieceImage, pieceType);
+			} else {
+				System.out.println("Drop failed: Same team pieces.");
+			}
+		} else {
+			System.out.println("Drop succeeded: Moved piece to empty cell.");
+			draw(pieceImage, pieceType);
+		}
+	}
 
-        switch (pieceType) {
-            case "pawnW":
-                return dx == 0 && dy == -1 && !chessPane.getCell(targetX, targetY).hasPiece();
-            case "rookW":
-                return (dx == 0 || dy == 0) && isPathClear(startX, startY, targetX, targetY, chessPane);
-            case "knightW":
-                return (Math.abs(dx) == 2 && Math.abs(dy) == 1) || (Math.abs(dx) == 1 && Math.abs(dy) == 2);
-            case "bishopW":
-                return Math.abs(dx) == Math.abs(dy) && isPathClear(startX, startY, targetX, targetY, chessPane);
-            case "queenW":
-                return (dx == 0 || dy == 0 || Math.abs(dx) == Math.abs(dy)) && isPathClear(startX, startY, targetX, targetY, chessPane);
-            case "kingW":
-                return Math.abs(dx) <= 1 && Math.abs(dy) <= 1;
-            default:
-                return false;
-        }
-    }
-	
+	private boolean validateMove(String pieceType, int startX, int startY, int targetX, int targetY,
+			ChessPane chessPane) {
+		int dx = targetX - startX;
+		int dy = targetY - startY;
+
+		switch (pieceType) {
+		case "pawnW":
+			return dx == 0 && dy == -1 && !chessPane.getCell(targetX, targetY).hasPiece();
+		case "rookW":
+			return (dx == 0 || dy == 0) && isPathClear(startX, startY, targetX, targetY, chessPane);
+		case "knightW":
+			return (Math.abs(dx) == 2 && Math.abs(dy) == 1) || (Math.abs(dx) == 1 && Math.abs(dy) == 2);
+		case "bishopW":
+			return Math.abs(dx) == Math.abs(dy) && isPathClear(startX, startY, targetX, targetY, chessPane);
+		case "queenW":
+			return (dx == 0 || dy == 0 || Math.abs(dx) == Math.abs(dy))
+					&& isPathClear(startX, startY, targetX, targetY, chessPane);
+		case "kingW":
+			return Math.abs(dx) <= 1 && Math.abs(dy) <= 1;
+		default:
+			return false;
+		}
+	}
+
 	private boolean isPathClear(int startX, int startY, int targetX, int targetY, ChessPane chessPane) {
-        int dx = Integer.compare(targetX, startX);
-        int dy = Integer.compare(targetY, startY);
+		int dx = Integer.compare(targetX, startX);
+		int dy = Integer.compare(targetY, startY);
 
-        int x = startX + dx;
-        int y = startY + dy;
+		int x = startX + dx;
+		int y = startY + dy;
 
-        while (x != targetX || y != targetY) {
-            if (chessPane.getCell(x, y).hasPiece()) {
-                return false;
-            }
-            x += dx;
-            y += dy;
-        }
+		while (x != targetX || y != targetY) {
+			if (chessPane.getCell(x, y).hasPiece()) {
+				return false;
+			}
+			x += dx;
+			y += dy;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
 }
